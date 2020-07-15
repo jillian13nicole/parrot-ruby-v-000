6d9db65522a7552a6dd564d4fe061ff5fced9ef7
Jillian Weimer <jilliannweimer@gmail.com>
2020-07-15 00:34:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(noise="Squawk!")
-  puts "Parrots make a sound like #{noise}"
+  puts noise
     return noise
 end
